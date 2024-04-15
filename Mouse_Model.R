062773246378307f0ab73d0aaa773b20bbe9299a
Rachel Larson, PhD <126201983+RLarson92@users.noreply.github.com>
2024-04-15 16:31:23 -0500

    Add files via upload

---
+++ Mouse_Model.R
@@ -0,0 +1,197 @@
+
+# load functions used to clean data
+functions_to_load <- list.files(
+  "./functions/",
+  full.names = TRUE
+)
+for(fn in functions_to_load){
+  source(fn)
+}
+# read in the mouse count dataset
+df <- read.csv("./data/PERO.csv")
+dets <- df[ ,2:19] # 2:19 for full dataset, 2:7 for 2-season test
+# stack the data, long-style
+stack <- wide_to_stacked(dets, 3)
+# stack the replicates (nights) on top of each other and order the datasheet. 
+# Data should be in order by season then site
+library(tidyr)
+n_long <- gather(stack, key = "night", value = "N", night1:night3, factor_key = TRUE)
+n_long <- n_long[order(
+  n_long[,"Season"],
+  n_long[,"Site"]), ]
+n_long$night <- as.numeric(n_long$night)
+n_long$Site <- as.numeric(as.factor(n_long$Site))
+
+# create a blank array that has the correct dimensions
+my_array <- array(
+  NA,
+  dim = c(
+    max(n_long$Site),
+    max(n_long$night),
+    max(n_long$Season)
+  )
+)
+# fill in the array with the information from the long-form data.frame
+for(i in 1:nrow(n_long)){
+  my_array[
+    n_long$Site[i],
+    n_long$night[i],
+    n_long$Season[i]
+  ] <- n_long$N[i]
+}
+rm(dets, stack)
+
+# Site covariates are a 2-D array [sites, covariates]
+site_covs <- read.csv("./data/siteCovs_Rod.csv", stringsAsFactors = FALSE)
+site_covs$site <- as.numeric(as.factor(site_covs$site))
+site_covs$cat.sd <- abs(site_covs$cat.sd)
+site_covs$fox.sd <- abs(site_covs$fox.sd)
+site_covs$coyote.sd <- abs(site_covs$coyote.sd)
+site_covs$mink.sd <- abs(site_covs$mink.sd)
+cor(site_covs)
+covMatrix <- as.matrix(site_covs)
+rm(site_covs)
+
+# Observation covariates are 3-D array [sites, nights, seasons]
+# We have 3 separate arrays, one for each covariate
+moon <- read.csv("./data/obsVars/Moon.csv", stringsAsFactors = FALSE)
+moon1 <- as.data.frame(moon[,2:19]) # to 19 for full dataset, to 10 for test
+moonStack <- wideObs_to_stacked(moon1, 3) 
+moon_long <- gather(moonStack, key = "night", value = "moon", night1:night3, factor_key = TRUE)
+moon_long$moon <- as.numeric(scale(moon_long$moon))
+moon_long <- moon_long[order(
+  moon_long[,"Season"],
+  moon_long[,"Site"]),]
+moon_long$night <- as.numeric(moon_long$night)
+moon_long$Site <- as.numeric(as.factor(moon_long$Site))
+moonArray <- array(
+  NA,
+  dim = c(max(moon_long$Site),
+          max(moon_long$night),
+          max(moon_long$Season)
+  )
+)
+for(i in 1:nrow(moon_long)){
+  moonArray[
+    moon_long$Site[i],
+    moon_long$night[i],
+    moon_long$Season[i]
+  ] <- moon_long$moon[i]
+}
+rm(moon1, moonStack, moon_long)
+
+jDate <- read.csv("./data/obsVars/jDate.csv", stringsAsFactors = FALSE)
+jDate1 <- as.data.frame(jDate[,2:19]) # to 19 for full dataset, to 10 for test
+jDateStack <- wideObs_to_stacked(jDate1, 3) 
+jDate_long <- gather(jDateStack, key = "night", value = "jDate", night1:night3, factor_key = TRUE)
+jDate_long$jDate <- as.numeric(scale(jDate_long$jDate))
+jDate_long <- jDate_long[order(
+  jDate_long[,"Season"],
+  jDate_long[,"Site"]),]
+jDate_long$night <- as.numeric(jDate_long$night)
+jDate_long$Site <- as.numeric(as.factor(jDate_long$Site))
+jDateArray <- array(
+  NA,
+  dim = c(max(jDate_long$Site),
+          max(jDate_long$night),
+          max(jDate_long$Season)
+  )
+)
+for(i in 1:nrow(jDate_long)){
+  jDateArray[
+    jDate_long$Site[i],
+    jDate_long$night[i],
+    jDate_long$Season[i]
+  ] <- jDate_long$jDate[i]
+}
+rm(jDate1, jDateStack, jDate_long, jDate)
+
+effort <- read.csv("./data/obsVars/Effort.csv", stringsAsFactors = FALSE)
+effort1 <- as.data.frame(effort[,2:19]) # to 19 for full dataset, to 10 for test
+effortStack <- wideObs_to_stacked(effort1, 3) 
+effort_long <- gather(effortStack, key = "night", value = "effort", night1:night3, factor_key = TRUE)
+effort_long$effort <- as.numeric(scale(effort_long$effort))
+effort_long <- effort_long[order(
+  effort_long[,"Season"],
+  effort_long[,"Site"]),]
+effort_long$night <- as.numeric(effort_long$night)
+effort_long$Site <- as.numeric(as.factor(effort_long$Site))
+effortArray <- array(
+  NA,
+  dim = c(max(effort_long$Site),
+          max(effort_long$night),
+          max(effort_long$Season)
+  )
+)
+for(i in 1:nrow(effort_long)){
+  effortArray[
+    effort_long$Site[i],
+    effort_long$night[i],
+    effort_long$Season[i]
+  ] <- effort_long$effort[i]
+}
+rm(effort1, effortStack, effort_long, effort, moon)
+
+seasonData <- as.factor(c("spring","summer"
+                          ,"fall","spring","summer","fall"
+                          ))
+
+#### RUN MODEL ####
+# Data list for model
+data_list <- list(
+  nsite = max(n_long$Site),
+  PC1 = covMatrix[,2],
+  contag = covMatrix[,3],
+  cat.mu = covMatrix[,4],
+  cat.sd = covMatrix[,5],
+  fox.mu = covMatrix[,8],
+  fox.sd = covMatrix[,9],
+  nNight = max(n_long$night),
+  moon = moonArray,
+  jDate = jDateArray,
+  effort = effortArray,
+  y = my_array, 
+  nSP = max(n_long$Season),
+  season = seasonData,
+  nseason = 3
+)
+save(data_list, file = "./results/data_list.Rdata") #not really a 'result' but saving there for later use
+
+# initial values for N should be maximum possible counts
+N_init = array(10, dim = c(data_list$nsite, data_list$nseason))
+# set to NA for all seasons t>1 because we cannot know this (needs to be specified by R + S)
+N_init[,2:6] <- NA
+# initial values for recruits can also be set to maximum possible counts
+R_init = array(10, dim = c(data_list$nsite, data_list$nseason))
+# providing null values for t = 1 (i.e., no recruits yet)
+R_init[,1] <- NA
+
+# Fit the model
+library(runjags)
+my_mod <- runjags::run.jags(
+  model = "./JAGS/mouseModel.R",
+  monitor = c("beta0","beta1",
+              "phi0", "phi1", "phi2", "phi3", "phi4", "phi5", "phi6",
+              "gamma0", "gamma1", "gamma2",
+              "alpha0", "alpha1", "alpha2", "alpha3",
+              "cat","fox"
+              ),
+  data = data_list,
+  n.chains = 3,
+  inits = rod_inits,
+  burnin = 100000,
+  sample = 25000,
+  adapt = 1000,
+  modules = "glm",
+  thin = 2,
+  method = "parallel"
+)
+system("say Calculations Complete.")
+saveRDS(my_mod, "./results/rod_model.RDS")
+summary(my_mod, vars = c("beta0","beta1",
+                         "phi0", "phi1", "phi2", "phi3", "phi4", "phi5", "phi6",
+                         "gamma0", "gamma1", "gamma2",
+                         "alpha0", "alpha1", "alpha2", "alpha3"
+))
+plot(my_mod)
+
---
+++ Predator_Models.R
@@ -0,0 +1,520 @@
+
+# load functions used to clean data
+functions_to_load <- list.files(
+  "./functions/",
+  full.names = TRUE
+)
+for(fn in functions_to_load){
+  source(fn)
+}
+# read in the full predator dataset with both species
+dat <- read.csv("./data/predatorOccu.csv")
+# converting the site names & species into unique numbers
+dat$Site <- as.numeric(as.factor(dat$Site))
+dat$Species <- as.numeric(as.factor(dat$Species))
+str(dat)
+# converting Y to NA if J (i.e., trap-days) is 0
+dat$Y[dat$J==0] <- NA
+# re-order the data to be in order by species, season, then site
+dat <- dat[order(
+  dat[,"Species"], 
+  dat[,"Season"],
+  dat[,"Site"]), ]
+# create blank array w/ correct dimensions then fill it in w/ detections
+y <- array(data = NA, dim = c(max(dat$Species), max(dat$Site), max(dat$Season)))
+for(i in 1:nrow(dat)){
+  y[
+    dat$Species[i],
+    dat$Site[i],
+    dat$Season[i]
+  ] <- dat$Y[i]
+}
+# create blank matrix w/ correct dimensions then fill it in w/ camera-Days
+J <- matrix(data = NA, nrow = max(dat$Site), ncol = max(dat$Season))
+for(i in 1:nrow(dat)){
+  J[
+    dat$Site[i],
+    dat$Season[i]
+  ] <- dat$J[i]
+}
+
+# reading in detection covariates & converting sites to numeric factors
+# obsCovs <- read.csv("./data/obsCovs_Pred.csv")
+# obsCovs <- subset(obsCovs, Site!="A09-KEN2")
+# obsCovs$Site <- as.numeric(as.factor(obsCovs$Site))
+# # turning 'season' (lower case meaning time of year of sampling [fall, spring, etc]) into a 
+# # factor w/ 4 levels
+# obsCovs$season <- as.factor(obsCovs$season)
+# str(obsCovs)
+# 
+# season <- matrix(data = NA, nrow = max(obsCovs$Site), ncol = max(obsCovs$Season))
+# for(i in 1:nrow(obsCovs)){
+#   season[
+#     obsCovs$Site[i],
+#     obsCovs$Season[i]
+#   ] <- obsCovs$season[i]
+# }
+seasonData <- as.factor(c("fall","winter","spring","summer",
+                          "fall","winter","spring","summer",
+                          "fall","winter","spring","summer",
+                          "fall"))
+
+# reading in site covariates & converting sites to numeric factors
+siteCovs <- read.csv("./data/siteCovs_Pred.csv")
+siteCovs$Site <- as.numeric(as.factor(siteCovs$Site))
+cor(scale(siteCovs))
+# re-order the data to be in order by site
+siteCovs <- siteCovs[order(siteCovs[,"Site"]), ]
+siteCovs$Imperv <- as.numeric(scale(siteCovs$Imperv))
+siteCovs$Forest <- as.numeric(scale(siteCovs$Forest))
+siteCovs$Prairie <- as.numeric(scale(siteCovs$Prairie))
+siteCovs$ResUnits <- as.numeric(scale(siteCovs$ResUnits))
+siteCovs$Crop <- as.numeric(scale(siteCovs$Crop))
+siteCovs$Dist_to_Wat <- as.numeric(scale(siteCovs$Dist_to_Wat))
+
+# last but not least, making the array of possible occupancy states
+# (e.g., 1/1/1/1 = all species present; 0/1/0/0 = only coyote present, etc.)
+Xcat <- matrix(c(1, 1, 1, 1,
+                 1, 0, 0, 0,
+                 0, 1, 0, 0,
+                 0, 0, 1, 0,
+                 0, 0, 0, 1,
+                 1, 1, 0, 0,
+                 1, 0, 1, 0,
+                 1, 0, 0, 1,
+                 0, 1, 1, 0,
+                 0, 1, 0, 1,
+                 0, 0, 1, 1,
+                 0, 0, 0, 0), ncol = 4, byrow = TRUE)
+
+#### RUN MODEL ####
+# Data list for model
+data_list <- list(
+  nspec = max(dat$Species),
+  nsite = max(dat$Site),
+  house = siteCovs$ResUnits,
+  crop = siteCovs$Crop,
+  forest = siteCovs$Forest,
+  prairie = siteCovs$Prairie,
+  water = siteCovs$Dist_to_Wat,
+  imperv = siteCovs$Imperv,
+  season = seasonData,
+  nseason = 4,
+  y = y, 
+  J = J,
+  Xcat = Xcat,
+  nSP = max(dat$Season)
+)
+# Fit the model
+library(runjags)
+pred_mod <- runjags::run.jags(
+  model = "./jags/predatorModel.R",
+  monitor = c("a0", "a1", "a2", "b0", "b1", "b2", "c0", "c1", "d0", "d1", "d2",
+              "e0", "e1", "g0", "g1", "h0", "h1",
+              "l0", "l1", "m0", "m1",
+              "n0", "n1",
+              "p0", "p1",
+              "phi",
+              "x"),
+  data = data_list,
+  n.chains = 3,
+  inits = pred_inits,
+  burnin = 1000,
+  sample = 5000,
+  adapt = 500,
+  modules = "glm",
+  thin = 2,
+  method = "parallel"
+)
+system("say Calculations Complete.")
+saveRDS(pred_mod, "./results/pred_model.RDS")
+summary(pred_mod,
+        vars = c("a0", "a1", "a2", "b0", "b1", "b2", "c0", "c1", "d0", "d1", "d2",
+                "e0", "e1", "g0", "g1", "h0", "h1", "l0", "l1", "m0", "m1", "n0", "n1",
+                "p0", "p1",
+                "phi"
+        ))
+plot(pred_mod)
+
+#### Post-Model Calculations ####
+# convert the model file to an mcmc object; use MCMCvis to get summary statistics
+library(coda)
+mc <- as.mcmc(pred_mod)
+library(MCMCvis)
+MCMCvis::MCMCsummary(mc,
+                     params = c("a0", "a1", "a2", "b0", "b1", "b2", "c0", "c1", "d0", "d1", "d2",
+                                "e0", "e1", "g0", "g1", "h0", "h1", "l0", "l1", "m0", "m1", "n0", "n1",
+                                "p0", "p1",
+                                "phi"
+                                ),
+                     probs = c(0.025, 0.5, 0.975), 
+                     round = 2)
+
+# convert the mcmc object to a matrix
+mc <- as.matrix(mc)
+# sub-sample the mcmc matrix a bit as we don't really need to make predictions with 
+# all 60K samples
+set.seed(554)
+mc_sub <- mc[sample(1:nrow(mc), 10000), ]
+# and use split_mcmc
+mc <- split_mcmc(mc_sub)
+rm(mc_sub)
+
+pred_psi <- array(
+  NA,
+  dim = c(
+    nrow(mc$a0),
+    ncol(1),
+    12
+  )
+)
+# no species
+pred_psi[,1] <- 1
+# Species 1 (cat)
+pred_psi[,2] <- exp(mc$a0[,1])
+# Species 2 (coyote)
+pred_psi[,3] <- exp(mc$b0[,1])
+# Species 3 (fox)
+pred_psi[,4] <- exp(mc$c0[,1])
+# Species 4 (mink)
+pred_psi[,5] <- exp(mc$d0[,1])
+# species 1 & 2
+pred_psi[,6] <- exp(mc$a0[,1] + mc$b0[,1] + mc$e0[,1])
+# species 1 & 3
+pred_psi[,7] <- exp(mc$a0[,1] + mc$c0[,1] + mc$g0[,1])
+# species 1 & 4
+pred_psi[,8] <- exp(mc$a0[,1] + mc$d0[,1] + mc$h0[,1])
+# species 2 & 3
+pred_psi[,9] <- exp(mc$b0[,1] + mc$c0[,1] + mc$l0[,1])
+# species 2 & 4
+pred_psi[,10] <- exp(mc$b0[,1] + mc$d0[,1] + mc$m0[,1])
+# species 3 & 4
+pred_psi[,11] <- exp(mc$c0[,1] + mc$d0[,1] + mc$n0[,1])
+# all species together
+pred_psi[,12] <- exp(mc$a0[,1] + mc$b0[,1] + mc$c0[,1] + mc$d0[,1] + 
+                       mc$e0[,1] + mc$g0[,1] + mc$h0[,1] + mc$l0[,1] + mc$m0[,1] + mc$n0[,1])
+# convert to probability
+prob_psi <- t(apply(pred_psi, 1, function(x) x/sum(x)))
+
+pred_psi_auto <- array(
+  NA,
+  dim = c(
+    nrow(mc$a0),
+    ncol(1),
+    12
+  )
+)
+# no species
+pred_psi_auto[,1] <- 1
+pred_psi_auto[,2] <- exp(mc$a0[,1] + mc$phi[,1])
+pred_psi_auto[,3] <- exp(mc$b0[,1] + mc$phi[,2])
+pred_psi_auto[,4] <- exp(mc$c0[,1] + mc$phi[,3])
+pred_psi_auto[,5] <- exp(mc$d0[,1] + mc$phi[,4])
+pred_psi_auto[,6] <- exp(mc$a0[,1] + mc$phi[,1] + mc$b0[,1] + mc$phi[,2] + mc$e0[,1])
+pred_psi_auto[,7] <- exp(mc$a0[,1] + mc$phi[,1] + mc$c0[,1] + mc$phi[,3] + mc$g0[,1])
+pred_psi_auto[,8] <- exp(mc$a0[,1] + mc$phi[,1] + mc$d0[,1] + mc$phi[,4] + mc$h0[,1])
+pred_psi_auto[,9] <- exp(mc$b0[,1] + mc$phi[,2] + mc$c0[,1] + mc$phi[,3] + mc$l0[,1])
+pred_psi_auto[,10] <- exp(mc$b0[,1] + mc$phi[,2] + mc$d0[,1] + mc$phi[,4] + mc$m0[,1])
+pred_psi_auto[,11] <- exp(mc$c0[,1] + mc$phi[,3] + mc$d0[,1] + mc$phi[,4] + mc$n0[,1])
+pred_psi_auto[,12] <- exp(mc$a0[,1] + mc$phi[,1] + mc$b0[,1] + mc$phi[,2] + mc$c0[,1] + 
+                            mc$phi[,3] + mc$d0[,1] + mc$phi[,4] +
+                       mc$e0[,1] + mc$g0[,1] + mc$h0[,1] + mc$l0[,1] + mc$m0[,1] + mc$n0[,1])
+# convert to probability
+prob_psi_auto <- t(apply(pred_psi, 1, function(x) x/sum(x)))
+
+# calculating average psi
+trueProb <- prob_psi / (
+  prob_psi + (1 - prob_psi_auto)
+)
+
+trueProb <- apply(
+  trueProb,
+  2,
+  quantile,
+  probs = c(0.025,0.25,0.5,0.75,0.975)
+)
+#### Marginal Occupancy ####
+Cat <- trueProb[,2] + trueProb[,6] + trueProb[,7] + trueProb[,8] + trueProb[,12]
+Coyote <- trueProb[,3] + trueProb[,6] + trueProb[,9] + trueProb[,10] + trueProb[,12]
+Fox <- trueProb[,4] + trueProb[,7] + trueProb[,9] + trueProb[,11] + trueProb[,12]
+Mink <- trueProb[,5] + trueProb[,8] + trueProb[,10] + trueProb[,11] + trueProb[,12]
+#### Conditional Occupancies ####
+catCoyote <- (trueProb[,6] + trueProb[,12]) / (trueProb[,6] + trueProb[,12] + trueProb[,3] + trueProb[,9] + trueProb[,10])
+catCoyote
+Cat*Coyote # cats & coyotes occur together MORE than expected
+# more likely to co-occur as imperv increases
+# likely b/c of increased probability of occurrence of cats?
+foxCoyote <- (trueProb[,9] + trueProb[,12]) / (trueProb[,9] + trueProb[,12] + trueProb[,3] + trueProb[,6] + trueProb[,10])
+foxCoyote
+Fox*Coyote # fox & coyotes occur together MORE than expected
+# less likely to co-occur as imperv increases
+# likely b/c foxes are more likely to occur as imperv increases, while coyotes avoid imperv
+minkCoyote <- (trueProb[,10] + trueProb[,12]) / (trueProb[,10] + trueProb[,12] + trueProb[,3] + trueProb[,6] + trueProb[,9])
+minkCoyote
+Mink*Coyote # mink & coyotes occur together AS EXPECTED
+
+#### Average Occupancy by Site
+x_array <- as.array(mc$x)
+pred_Site <- array(
+  NA,
+  dim = c(
+    2,
+    max(data_list$nsite),
+    max(data_list$nspec)
+  )
+)
+# cat marginal occupancy
+cat <- replace(x_array, x_array == 12, 0)
+cat <- replace(cat, cat == 11, 0)
+cat <- replace(cat, cat == 10, 0)
+cat <- replace(cat, cat == 9, 0)
+cat <- replace(cat, cat == 5, 0)
+cat <- replace(cat, cat == 4, 0)
+cat <- replace(cat, cat == 3, 0)
+cat <- replace(cat, cat == 2, 1)
+cat <- replace(cat, cat == 6, 1)
+cat <- replace(cat, cat == 7, 1)
+cat <- replace(cat, cat == 8, 1)
+pred_Site[1,,1] <- apply(cat, 2, mean)
+pred_Site[2,,1] <- apply(cat, 2, sd)
+# coyote marginal occupancy
+coyote <- replace(x_array, x_array == 12, 0)
+coyote <- replace(coyote, coyote == 11, 0)
+coyote <- replace(coyote, coyote == 8, 0)
+coyote <- replace(coyote, coyote == 7, 0)
+coyote <- replace(coyote, coyote == 5, 0)
+coyote <- replace(coyote, coyote == 4, 0)
+coyote <- replace(coyote, coyote == 2, 0)
+coyote <- replace(coyote, coyote == 3, 1)
+coyote <- replace(coyote, coyote == 6, 1)
+coyote <- replace(coyote, coyote == 9, 1)
+coyote <- replace(coyote, coyote == 10, 1)
+pred_Site[1,,2] <- apply(coyote, 2, mean)
+pred_Site[2,,2] <- apply(coyote, 2, sd)
+# fox marginal occupancy
+fox <- replace(x_array, x_array == 12, 0)
+fox <- replace(fox, fox == 10, 0)
+fox <- replace(fox, fox == 8, 0)
+fox <- replace(fox, fox == 6, 0)
+fox <- replace(fox, fox == 5, 0)
+fox <- replace(fox, fox == 2, 0)
+fox <- replace(fox, fox == 3, 0)
+fox <- replace(fox, fox == 4, 1)
+fox <- replace(fox, fox == 7, 1)
+fox <- replace(fox, fox == 9, 1)
+fox <- replace(fox, fox == 11, 1)
+pred_Site[1,,3] <- apply(fox, 2, mean)
+pred_Site[2,,3] <- apply(fox, 2, sd)
+# mink marginal occupancy
+mink <- replace(x_array, x_array == 12, 0)
+mink <- replace(mink, mink == 9, 0)
+mink <- replace(mink, mink == 7, 0)
+mink <- replace(mink, mink == 6, 0)
+mink <- replace(mink, mink == 2, 0)
+mink <- replace(mink, mink == 3, 0)
+mink <- replace(mink, mink == 4, 0)
+mink <- replace(mink, mink == 5, 1)
+mink <- replace(mink, mink == 8, 1)
+mink <- replace(mink, mink == 10, 1)
+mink <- replace(mink, mink == 11, 1)
+pred_Site[1,,4] <- apply(mink, 2, mean)
+pred_Site[2,,4] <- apply(mink, 2, sd)
+# convert from sd to var and transpose dataframe
+# pred_Site <- apply(pred_Site, 2, function(x) x^2)
+c <- pred_Site[,,1]
+c <- t(c)
+y <- pred_Site[,,2]
+y <- t(y)
+x <- pred_Site[,,3]
+x <- t(x)
+m <- pred_Site[,,4]
+m <- t(m)
+occuSitePreds <- cbind(c,y,x,m)
+colnames(occuSitePreds) <- c("cat.mu","cat.sd","coyote.mu","coyote.sd",
+                             "fox.mu","fox.sd","mink.mu","mink.sd")
+write.csv(occuSitePreds, "./data/modeledOccu_Predators.csv")
+
+#### detection probability for cats ####
+# Generating a matrix that will contain values for predicting. This includes our sequence of
+# contag values, and adding a row of 1's for the intercept & other predictor values
+predV <- c(1,2,3,4)
+pred_mat <- cbind(
+  1,
+  predV
+)
+pred_mat <- t(pred_mat)
+# matrix should be long; ncol = length of your predictor sequence, 
+#                        nrow = # covariates + intercept (remember to use average values for all
+#                           covariates except your variable of interest)
+
+# create the matrix of the coefficients from the model
+pred_p <- array(
+  NA,
+  dim = c(
+    nrow(mc$p0),
+    ncol(pred_mat)
+  )
+)
+
+# dimensions should be: number of MCMC interations by length of sequence of predictor variable
+# by number of species
+# fill it in
+pred_p <- cbind(mc$p0[,2],mc$p1[,2]) %*% pred_mat
+
+# convert to probability
+prob_p <- apply(pred_p,
+                c(2),
+                logit2prob)
+prob_p <- apply(
+  prob_p,
+  c(2),
+  quantile,
+  probs = c(0.025,0.25,0.5,0.75,0.975)
+)
+
+#### coyote + fox imperv relationship ####
+predV <- seq(-1.15, 2.47,length.out=200)
+pred_mat <- cbind(
+  1,
+  predV
+)
+pred_mat <- t(pred_mat)
+
+pred_psi <- array(
+  NA,
+  dim = c(
+    nrow(mc$a0),
+    ncol(pred_mat),
+    12
+  )
+)
+
+# no species
+pred_psi[,,1] <- 1
+# Species 1 (cat)
+pred_psi[,,2] <- exp(mc$a0[,1])
+# Species 2 (coyote)
+pred_psi[,,3] <- exp(mc$b0[,1])
+# Species 3 (fox)
+pred_psi[,,4] <- exp(mc$c0[,1])
+# Species 4 (mink)
+pred_psi[,,5] <- exp(mc$d0[,1])
+# species 1 & 2
+pred_psi[,,6] <- exp(mc$a0[,1] + mc$b0[,1] + 
+                       cbind(mc$e0[,1],mc$e1[,1]) %*% pred_mat)
+# species 1 & 3
+pred_psi[,,7] <- exp(mc$a0[,1] + mc$c0[,1] + 
+                       cbind(mc$g0[,1],mc$g1[,1]) %*% pred_mat)
+# species 1 & 4
+pred_psi[,,8] <- exp(mc$a0[,1] + mc$d0[,1] + 
+                       cbind(mc$h0[,1],mc$h1[,1]) %*% pred_mat)
+# species 2 & 3
+pred_psi[,,9] <- exp(mc$b0[,1] + mc$c0[,1] + 
+                      cbind(mc$l0[,1],mc$l0[,1]) %*% pred_mat)
+# species 2 & 4
+pred_psi[,,10] <- exp(mc$b0[,1] + mc$d0[,1] + 
+                       cbind(mc$m0[,1],mc$m1[,1]) %*% pred_mat)
+# species 3 & 4
+pred_psi[,,11] <- exp(mc$c0[,1] + mc$d0[,1] + 
+                       cbind(mc$n0[,1],mc$n1[,1]) %*% pred_mat)
+# all species together
+pred_psi[,,12] <- exp(mc$a0[,1] + mc$b0[,1] + mc$c0[,1] + mc$d0[,1] + 
+                        cbind(mc$e0[,1],mc$e1[,1]) %*% pred_mat + 
+                        cbind(mc$g0[,1],mc$g1[,1]) %*% pred_mat + 
+                        cbind(mc$h0[,1],mc$h1[,1]) %*% pred_mat + 
+                        cbind(mc$l0[,1],mc$l0[,1]) %*% pred_mat + 
+                        cbind(mc$m0[,1],mc$m1[,1]) %*% pred_mat + 
+                        cbind(mc$n0[,1],mc$n1[,1]) %*% pred_mat)
+# convert to probability
+prob_psi <- array(
+  NA,
+  dim = dim(pred_psi)
+)
+pb <- txtProgressBar(max = ncol(pred_mat))
+for(i in 1:ncol(pred_mat)){
+  setTxtProgressBar(pb, i)
+  prob_psi[,i,] <- sweep(
+    pred_psi[,i,],
+    1,
+    rowSums(pred_psi[,i,]),
+    FUN = "/"
+  )
+}
+
+# no species
+pred_psi_auto[,,1] <- 1
+pred_psi_auto[,,2] <- exp(mc$a0[,1] + mc$phi[,1])
+pred_psi_auto[,,3] <- exp(mc$b0[,1] + mc$phi[,2])
+pred_psi_auto[,,4] <- exp(mc$c0[,1] + mc$phi[,3])
+pred_psi_auto[,,5] <- exp(mc$d0[,1] + mc$phi[,4])
+pred_psi_auto[,,6] <- exp(mc$a0[,1] + mc$phi[,1] + mc$b0[,1] + mc$phi[,2] + 
+                            cbind(mc$e0[,1],mc$e1[,1]) %*% pred_mat)
+pred_psi_auto[,,7] <- exp(mc$a0[,1] + mc$phi[,1] + mc$c0[,1] + mc$phi[,3] + 
+                            cbind(mc$g0[,1],mc$g1[,1]) %*% pred_mat)
+pred_psi_auto[,,8] <- exp(mc$a0[,1] + mc$phi[,1] + mc$d0[,1] + mc$phi[,4] + 
+                            cbind(mc$h0[,1],mc$h1[,1]) %*% pred_mat)
+pred_psi_auto[,,9] <- exp(mc$b0[,1] + mc$phi[,2] + mc$c0[,1] + mc$phi[,3] + 
+                            cbind(mc$l0[,1],mc$l1[,1]) %*% pred_mat)
+pred_psi_auto[,,10] <- exp(mc$b0[,1] + mc$phi[,2] + mc$d0[,1] + mc$phi[,4] + 
+                             cbind(mc$m0[,1],mc$m1[,1]) %*% pred_mat)
+pred_psi_auto[,,11] <- exp(mc$c0[,1] + mc$phi[,3] + mc$d0[,1] + mc$phi[,4] + 
+                             cbind(mc$n0[,1],mc$n1[,1]) %*% pred_mat)
+pred_psi_auto[,,12] <- exp(mc$a0[,1] + mc$phi[,1] + mc$b0[,1] + mc$phi[,2] + mc$c0[,1] + 
+                            mc$phi[,3] + mc$d0[,1] + mc$phi[,4] +
+                             cbind(mc$e0[,1],mc$e1[,1]) %*% pred_mat + 
+                             cbind(mc$g0[,1],mc$g1[,1]) %*% pred_mat + 
+                             cbind(mc$h0[,1],mc$h1[,1]) %*% pred_mat + 
+                             cbind(mc$l0[,1],mc$l1[,1]) %*% pred_mat + 
+                             cbind(mc$m0[,1],mc$m1[,1]) %*% pred_mat + 
+                             cbind(mc$n0[,1],mc$n1[,1]) %*% pred_mat)
+# convert to probability
+prob_psi_auto <- array(
+  NA,
+  dim = dim(pred_psi_auto)
+)
+pb <- txtProgressBar(max = ncol(pred_mat))
+for(i in 1:ncol(pred_mat)){
+  setTxtProgressBar(pb, i)
+  prob_psi_auto[,i,] <- sweep(
+    pred_psi_auto[,i,],
+    1,
+    rowSums(pred_psi_auto[,i,]),
+    FUN = "/"
+  )
+}
+
+# calculating average psi
+trueProb <- prob_psi / (
+  prob_psi + (1 - prob_psi_auto)
+)
+
+trueProb <- apply(
+  trueProb,
+  c(2,3),
+  quantile,
+  probs = c(0.025,0.25,0.5,0.75,0.975)
+)
+
+coyFox <- rbind(data.frame(imperv = seq(-1.15, 2.47,length.out=200),
+                            psi = ((trueProb[3,,3] + trueProb[3,,6] + trueProb[3,,9] + trueProb[3,,10] + trueProb[3,,12])*(trueProb[3,,4] + trueProb[3,,7] + trueProb[3,,9] + trueProb[3,,11] + trueProb[3,,12])),
+                            upper = ((trueProb[5,,3] + trueProb[5,,6] + trueProb[5,,9] + trueProb[5,,10] + trueProb[5,,12])*(trueProb[5,,4] + trueProb[5,,7] + trueProb[5,,9] + trueProb[5,,11] + trueProb[5,,12])),
+                            lower = ((trueProb[1,,3] + trueProb[1,,6] + trueProb[1,,9] + trueProb[1,,10] + trueProb[1,,12])*(trueProb[1,,4] + trueProb[1,,7] + trueProb[1,,9] + trueProb[1,,11] + trueProb[1,,12])),
+                            state = "Expected Co-occurrence"),
+                 data.frame(imperv = seq(-1.15, 2.47,length.out=200),
+                            psi = (trueProb[3,,9] + trueProb[3,,12]) / (trueProb[3,,3] + trueProb[3,,6] + trueProb[3,,9] + trueProb[3,,10] + trueProb[3,,12]),
+                            lower = (trueProb[1,,9] + trueProb[1,,12]) / (trueProb[1,,3] + trueProb[1,,6] + trueProb[1,,9] + trueProb[1,,10] + trueProb[1,,12]),
+                            upper = (trueProb[5,,9] + trueProb[5,,12]) / (trueProb[5,,3] + trueProb[5,,6] + trueProb[5,,9] + trueProb[5,,10] + trueProb[5,,12]),
+                            state = "Actual Co-occurrence")
+)
+
+ggplot(coyFox, aes(x=imperv, y=psi)) +
+  geom_ribbon(aes(ymin=lower, ymax=upper, fill=state), alpha=.3, show.legend=FALSE) +
+  scale_fill_manual(values=c("gray30","gray60"), guide = "none") +
+  geom_line(aes(color=state, linetype=state), linewidth=1.05, show.legend = FALSE) +
+  scale_color_manual(values=c("gray30","gray60"), guide = "none") +
+  scale_x_continuous(expand = c(0,0)) +
+  scale_y_continuous(limits = c(0,1), expand = c(0,0)) +
+  labs(title="", x="", y="", linetype="") +
+  theme_bw() +
+  theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank(),
+        axis.text=element_text(size=10))
